16043fb66bb5d5e4d2c48f72fc499d00f649ca98
Kau <kauanesdrk@gmail.com>
2025-02-08 01:06:46 -0300

    (style): increase gap between projects

--- src/Components/Tech/Tech.jsx
+++ src/Components/Tech/Tech.jsx
@@ -1,10 +1,24 @@
 import "./Tech.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as faIcons from '@fortawesome/free-brands-svg-icons';
-
+import * as fabIcons from '@fortawesome/free-brands-svg-icons';
+import * as faIcons from '@fortawesome/free-solid-svg-icons';
+import * as farIcons from '@fortawesome/free-regular-svg-icons';
 
 function Tech( { iconName, color, name } ) {
-    const icon = faIcons[iconName];
+    let icon = null;
+
+    if (fabIcons[iconName]) {
+      icon = fabIcons[iconName];
+    } else if (faIcons[iconName]) {
+      icon = faIcons[iconName];
+    } else if (farIcons[iconName]) {
+      icon = farIcons[iconName];
+    }
+
+    if (!icon) {
+        console.error(`Ícone ${iconName} não encontrado em nenhum pacote`);
+        return <div><p>Ícone não encontrado</p></div>;
+    }
     
     return (
 
